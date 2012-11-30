0ca0b95800b4219d3c0dbd17a5aadc59543b8dd1
Devin Lauren Elder <laurenelder@fullsail.edu>
2012-11-29 21:05:24 -0500

    Completed Project 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", function() {
-var localStorage;
+
 // Shortcut Function
 	var $ = function(x) {
 		var theElement = document.getElementById(x);
@@ -8,67 +8,71 @@ var localStorage;
 
 // Radio Button Functions
 	var getRadioGender = function() {
-		var radioOne = $("gender");
-		for (var i = radioOne.length - 1; i >= 0; i--) {
+		var radioOne = document.forms[0].gender;
+		for (var i = 0; i < radioOne.length; i++) {
 			if (radioOne[i].checked) {
 				genderValue = radioOne[i].value;
-				return genderValue;
-			}
+			} else {
+				genderValue = "Unknown"
+			};
 		};
-	}
+	};
 	var getRadioOrientation = function() {
-		var radioTwo = $("orientation");
-		for (var i = radioTwo.length - 1; i >= 0; i--) {
+		var radioTwo = document.forms[0].orientation;
+		for (var i = 0; i < radioTwo.length; i++) {
 			if (radioTwo[i].checked) {
 				orientationValue = radioTwo[i].value;
-				return orientationValue;
-			}
+			} else {
+				orientationValue = "Unknown"
+			};
 		};
-	}
+	};
 
 // Check Box Function
 	getCheckBoxValue = function() {
 		if ($("vehicle").checked) {
-			vehicleValue = $("vehicle").value
+			vehicleValue = "Yes";
 		} else {
 			vehicleValue = "No"
-		}
+		};
 		if ($("shared interests").checked) {
-			interestsValue = $("shared interests").value
+			interestsValue = "Yes";
 		} else {
-			interestsValue = "No"
-		}
+			interestsValue = "No";
+		};
 		if ($("financially stable").checked) {
-			stableValue = $("financially stable").value
+			stableValue = "Yes";
 		} else {
-			stableValue = "No"
-		}
+			stableValue = "No";
+		};
 		if ($("drinks").checked) {
-			drinksValue = $("drinks").value
+			drinksValue = "Yes";
 		} else {
-			drinksValue = "No"
-		}
+			drinksValue = "No";
+		};
 		if ($("smokes").checked) {
-			smokesValue = $("smokes").value
+			smokesValue = "Yes";
 		} else {
-			smokesValue = "No"
-		}
+			smokesValue = "No";
+		};
 		if ($("has kids").checked) {
-			hasKidsValue = $("has kids").value
+			hasKidsValue = "Yes";
 		} else {
-			hasKidsValue = "No"
-		}
+			hasKidsValue = "No";
+		};
 		if ($("wants kids").checked) {
-			wantsKidsValue = $("wants kids").value
+			wantsKidsValue = "Yes";
 		} else {
-			wantsKidsValue = "No"
-		}
+			wantsKidsValue = "No";
+		};
 		if ($("has pets").checked) {
-			hasPetsValue = $("has pets").value
+			hasPetsValue = "Yes";
 		} else {
-			hasPetsValue = "No"
-		}
-	}
+			hasPetsValue = "No";
+		};
+	};
+
+// Toggle links Function
 	var toggleLinks = function(n) {
 		switch(n) {
 			case "on":
@@ -86,52 +90,50 @@ var localStorage;
 				break;
 			default:
 				return false;
-		}
-	}
+		};
+	};
 
 // Get Data Function
 	var getData	= function() {
+		toggleLinks("on");
 		var makeDiv = document.createElement("div");
-		if (localStorage === 0) {
-			alert("No users to display.")
-		} else {
-			toggleLinks("on");
-			var makeDiv = document.createElement("div");
-			makeDiv.setAttribute("idNum", "item");
-			var makeList = document.createElement("ul");
-			makeDiv.appendChild(makeList);
-			document.body.appendChild(makeDiv);
-			for (var i = 0, j = localStorage.length; i < j; i++) {
-				var makeLi = document.createElement("li");
-				makeList.appendChild(makeLi);
-				var key = localStorage.key(i);
-				var value = localStorage.getItem(key);
-				var obj = JSON.parse(value);
-				makeLi.appendChild(makeSubList);
-				for(var n in obj) {
-					var makeSubList = document.createElement("li");
-					var optSubText = obj[n][0] + " " + obj[n][1];
-					makeSubList.innerHTML = optSubText;
-				}
+		makeDiv.setAttribute("id", "item");
+		var makeList = document.createElement("ul");
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		$("item").style.display = "block";
+		for (var i = 0, j = window.localStorage.length; i < j; i++) {
+			var makeLi = document.createElement("li");
+			makeList.appendChild(makeLi);
+			var key = window.localStorage.key(i);
+			var value = window.localStorage.getItem(key);
+			var obj = JSON.parse(value);
+			var makeSubList = document.createElement("ul");
+			makeLi.appendChild(makeSubList);
+			for(var n in obj) {
+				var makeSubLi = document.createElement("li");
+				makeSubList.appendChild(makeSubLi);
+				var optSubText = obj[n][0] + " " + obj[n][1];
+				makeSubLi.innerHTML = optSubText;
 			};
 		};
-	}
+	};
 
 // Store Data Function
 	var	storeData = function() {
-		var idNum				= Math.floor(Math.random()*1000001);
+		var id					= Math.floor(Math.random() * 1000001);
 		getRadioGender();
 		getRadioOrientation();
-		//getCheckBoxValue();
+		getCheckBoxValue();
 		var item				= {};
-			item.website		= ["Dating Website: ", $("wsite").value];
 			item.userName		= ["Username: ", $("uname").value];
 			item.age			= ["Age: ", $("age").value];
 			item.city			= ["City: ", $("city").value];
+			item.website		= ["Dating Website: ", $("wsite").value];
 			item.contacted		= ["First Contacted: ", $("fcontact").value];
-			item.gender			= ["Gender: ", getRadioGender.genderValue];
-			item.orientation	= ["Sexual Orientation: ", getRadioOrientation.orientationValue];
-		/*	item.vehicle		= ["Has a Vehicle: ", getCheckBoxValue.vehicleValue];
+			item.gender			= ["Gender: ", genderValue];
+			item.orientation	= ["Sexual Orientation: ", orientationValue];
+			item.vehicle		= ["Has a Vehicle: ", vehicleValue];
 			item.interests		= ["Has Shared Interests: ", interestsValue];
 			item.stable			= ["Is Financially Stable: ", stableValue];
 			item.drinks			= ["Drinks: ", drinksValue];
@@ -139,64 +141,42 @@ var localStorage;
 			item.hasKids		= ["Has Kids: ", hasKidsValue];
 			item.wantsKids		= ["Wants Kids: ", wantsKidsValue];
 			item.hasPets		= ["Has Pets: ", hasPetsValue];
-			*/
 			item.music			= ["Favorite Music Genre: ", $("music").value];
 			item.movie			= ["Favorite Movie Genre: ", $("movie").value];
 			item.compatible		= ["Level of Compatibility: ", $("compatibility").value];
 			item.notes			= ["Additional Information: ", $("comments").value];
-		var items = JSON.stringify(item)
-		localStorage.setItem(idNum, items);
-		alert("User Saved!")
-	}
+		
+		window.localStorage.setItem(id, JSON.stringify(item));
+		alert("User Saved!");
+	};
 
+// Clear Data Function
 	var clearLocal = function() {
-		if (localStorage.length === 0) {
-			alert("There is no data to clear.")
-		} else {
-			localStorage.clear();
+			window.localStorage.clear();
 			alert("All users deleted.");
 			window.location.reload();
-			return false;
-		}
-	}
+			return false;	
+	};
 
 // Delete users confirm function
-	var ask = function() {
-		var ask = confirm("Delete all users?");
-		if (ask) {
-			clearLocal();
+	var confirmDelete = function() {
+		if (window.localStorage.length === 0) {
+			alert("There is no data to clear.");
 		} else {
-			alert("Users not deleted.")
-		}
-	}
-
-// Enable disabled fields function
-	var enableFields = function(y) {
-		if (y != "") {
-			$("age").removeAttribute("disabled", "disabled");
-			$("city").removeAttribute("disabled", "disabled");
-		}
-	} 
-
-	// Variables
-	var	textFieldOne = $("wsite");
-	var	textFieldTwo = $("uname");
-	var	textFieldThree = $("age");
-	var	textFieldFour = $("city");
-	localStorage
-	
-
+			var ask = confirm("Delete all users?");
+			if (ask) {
+				clearLocal();
+			} else {
+				alert("Users not deleted.");
+			};
+		};
+	};
 
 // Event Listeners
 	var displayLink = $("display");
 	displayLink.addEventListener("click", getData);
 	var clearLink = $("clear");
-	clearLink.addEventListener("click", ask);
+	clearLink.addEventListener("click", confirmDelete);
 	var save = $("submit");
 	save.addEventListener("click", storeData);
-	var eFields = $("uname");
-	eFields.addEventListener("blur", enableFields)
-
-	
-
 });
